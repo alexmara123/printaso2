44e7519f422a1447645767323eb0913c1799b6c2
alexmara123 <mara.alexandru@gmail.com>
2025-04-26 21:58:14 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Printaso. All Rights Reserved.</p>
+      <div className="social-links">
+        <a href="https://twitter.com/printaso" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://discord.gg/printaso" target="_blank" rel="noopener noreferrer">Discord</a>
+        <a href="https://telegram.me/printaso" target="_blank" rel="noopener noreferrer">Telegram</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
